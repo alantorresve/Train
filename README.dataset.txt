d51f3abaf8b321cd37290c036e58dee216584c8c
alantorresve <alantorresve@gmail.com>
2024-04-01 15:32:39 +0800

    Train Yolov8nPose model

---
+++ README.dataset.txt
@@ -0,0 +1,6 @@
+# cubekeypoints > 2024-04-01 3:13pm
+https://universe.roboflow.com/capstone-umjl3/cubekeypoints
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ train.py
@@ -0,0 +1,12 @@
+import os
+import cv2
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    # Load a model
+    model = YOLO('yolov8n-pose.yaml')
+
+    # Train the model
+    results = model.train(data='data.yaml', epochs=150, imgsz=640, device='cuda', single_cls=True, batch=8)
+
+
